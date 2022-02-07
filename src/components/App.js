1b8b1c1b3956e3cb7a688b12feb5ef1540c47914
Rebekah Zhou <rebekahzhou@gmail.com>
2022-02-07 15:51:10 -0800

    commit tree d407f689b3a5b24f7908c2bdc895cf76f51579d1

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+import React, { useState, useEffect} from 'react';
+
+function App() {
+    const [isLoaded, setIsLoaded] = useState(false)
+    const [image, setImage] = useState("")
+
+  useEffect(() => {
+      fetch('https://dog.ceo/api/breeds/image/random')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            setImage(data.message)
+            setIsLoaded(true)
+        })
+  }, [])
+
+  if (!isLoaded) return <p>Loading...</p>
+  return <img src={image} alt="A Random Dog" />
+}
+
+export default App;
